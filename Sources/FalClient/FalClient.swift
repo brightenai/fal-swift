a0a2cffa1f9ede70c7db3ec9edaf9af3f978e54d
John Burkey <johnburkey@mac.com>
2024-09-18 00:51:54 -0500

    linux

--- Sources/FalClient/FalClient.swift
+++ Sources/FalClient/FalClient.swift
@@ -58,6 +58,7 @@ public struct FalClient: Client {
         let start = Int64(Date().timeIntervalSince1970 * 1000)
         var elapsed: Int64 = 0
         var isCompleted = false
+        
         while elapsed < timeout.milliseconds {
             let update = try await queue.status(app, of: requestId, includeLogs: includeLogs)
             if let onQueueUpdateCallback = onQueueUpdate {
@@ -67,8 +68,14 @@ public struct FalClient: Client {
             if isCompleted {
                 break
             }
+            
+            let startTime = Date()
             try await Task.sleep(nanoseconds: UInt64(Int(pollInterval.milliseconds * 1_000_000)))
             elapsed = Int64(Date().timeIntervalSince1970 * 1000) - start
+
+            let endTimeX = Date().timeIntervalSince(startTime)
+            print("waitedXXXX \(endTimeX)")
+
         }
         if !isCompleted {
             throw FalError.queueTimeout
